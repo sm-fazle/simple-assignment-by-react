46ed7394d1054a2233fd8b1a2f379ce5186054c1
Assignment01 <Fazle-Rabbi404@gmail.com>
2025-07-26 07:40:00 +0600

    new code added

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ favorite.reduce((sum, item) => sum + item.currentBidPrice, 0);
         <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 mt-11  ml-32">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
 </svg>
-<h2 className='text-[#102A53] font-bold capitalize text-2xl mt-10 mb-5 '>favorite items</h2>
+<h2 className='text-[#102A53] font-bold capitalize text-xl mt-10 mb-5 '>favorite items</h2>
 
     </div>
    
